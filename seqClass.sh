90705057a35b3eed3a6b71eb5cb151a7663e7f1e
Blanca <blanca.rodriguez@uvic.cat>
2020-02-13 08:37:58 -0800

    Comments added to each step

--- seqClass.sh
+++ seqClass.sh
@@ -2,15 +2,20 @@
 seq=$1
 seq=$(echo $seq | tr a-z A-Z) 
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
+#sequence can have these letters
   if [[ $seq =~ T ]]; then
     echo "The sequence is DNA"
+#if there is a T is DNA
   elif [[ $seq =~ U ]]; then
     echo "The sequence is RNA"
+#if there is a U is RNA
   else
     echo "The sequence can be DNA or RNA"
+#if there T and U can be both
   fi
 else
   echo "The sequence is not DNA nor RNA"
+#if there isnt any letter 
 fi
 motif=$(echo $2 | tr a-z A-Z)
 if [[ -n $motif ]]; then
