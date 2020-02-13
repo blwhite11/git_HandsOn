2635b96210578b14bb46318d8bdd6dc23db9a645
Blanca <blanca.rodriguez@uvic.cat>
2020-02-13 07:17:08 -0800

    Edited to covert to upper-case

---
+++ seqClass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+seq=$1
+seq=$(echo $seq | tr a-z A-Z) 
+if [[ $seq =~ ^[ACGTU]+$ ]]; then
+  if [[ $seq =~ T ]]; then
+    echo "The sequence is DNA"
+  elif [[ $seq =~ U ]]; then
+    echo "The sequence is RNA"
+  else
+    echo "The sequence can be DNA or RNA"
+  fi
+else
+  echo "The sequence is not DNA nor RNA"
+fi
