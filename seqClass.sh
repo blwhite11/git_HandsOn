07de0dde2a01bb82348aa73088b8164c13819169
Blanca <blanca.rodriguez@uvic.cat>
2020-02-13 07:39:43 -0800

    Revert "Remove"
    This reverts commit 30f886c1d36b3cb3837d5eeb9313cef719e8bc0c.

--- seqClass.sh
+++ seqClass.sh
@@ -3,7 +3,7 @@ seq=$1
 seq=$(echo $seq | tr a-z A-Z) 
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
   if [[ $seq =~ T ]]; then
-
+    echo "The sequence is DNA"
   elif [[ $seq =~ U ]]; then
     echo "The sequence is RNA"
   else
